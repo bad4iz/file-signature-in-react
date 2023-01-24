c9382de6db9bd9e0eb24a075587b4157875f4960
bad4iz <developer@bad4iz.ru>
2023-01-24 18:16:33 +0400

    vitest covarage #48

---
+++ src/te/sum.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+
+import { sum } from './sum'
+
+describe('file sum', () => {
+  it('should', () => {
+    expect.hasAssertions()
+    // Arrange (всякие моки)
+
+    // Act
+    const result = sum(1, 3)
+
+    // Assert
+    expect(result).toBe(4)
+  })
+})
