683018caa35dbf6ebe6bc60ec6641d29579bd12d
bad4iz <developer@bad4iz.ru>
2023-01-24 21:16:31 +0400

    ss

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,8 @@ exports["default"] = (0, vite_1.defineConfig)({
     test: {
         coverage: {
             // you can include other reporters, but 'json-summary' is required, json is recommended
-            reporter: ['html', 'text', 'json-summary', 'json']
+            reporter: ['html', 'text', 'json-summary', 'json'],
+            all: true
         }
     }
 });
