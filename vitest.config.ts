c226f9351b21bce9a0d4104a38222b5ca719504b
bad4iz <developer@bad4iz.ru>
2023-01-24 21:16:02 +0400

    d

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['html', 'text', 'json-summary', 'json'],
+      provider: 'istanbul', // or 'c8'
+      all: true,
     },
   },
 })
