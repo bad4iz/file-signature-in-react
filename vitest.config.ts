f25562481e31223e3d436f2b236477dcfa9e8509
bad4iz <developer@bad4iz.ru>
2023-02-12 15:52:08 +0400

    import { defineConfig } from 'vitest/config'

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['json-summary'],
+      reporter: ['html', 'text', 'json-summary', 'json'],
       all: true,
     },
   },
