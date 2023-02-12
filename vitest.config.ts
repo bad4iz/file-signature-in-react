22df43fd9a1836a689cd5dbdfd5a5d0c90521b8a
bad4iz <developer@bad4iz.ru>
2023-02-12 15:50:09 +0400

    json sum

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['html', 'text', 'json-summary', 'json'],
+      reporter: ['json-summary'],
       all: true,
     },
   },
-})
+});
