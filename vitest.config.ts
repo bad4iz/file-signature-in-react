326101418591360545c3bd5f11511196f36f2674
bad4iz <developer@bad4iz.ru>
2023-04-23 09:19:30 +0400

    #29: happi dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    environment: 'happy-dom', // or 'jsdom', 'node'
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['json-summary', 'text', 'html', 'json', 'cobertura'],
