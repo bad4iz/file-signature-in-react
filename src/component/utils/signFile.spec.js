a22ba1d765bd2236ac1b5fc50fad45e094ec3852
bad4iz <developer@bad4iz.ru>
2024-01-21 00:58:39 +0400

    #20: start test

--- src/component/utils/signFile.spec.js
+++ src/component/utils/signFile.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { signFile } from './signFile'; // Replace 'yourFileName' with the actual name of your TypeScript file
 
