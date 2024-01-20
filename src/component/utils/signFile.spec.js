1d1c720bbcd96a43c46831f553b45209a902bced
bad4iz <developer@bad4iz.ru>
2024-01-21 00:57:51 +0400

    #20: start test

---
+++ src/component/utils/signFile.spec.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+
+import { signFile } from './signFile'; // Replace 'yourFileName' with the actual name of your TypeScript file
+
+describe('signFile.spec', () => {
+  it('default', () => {
+    expect.hasAssertions();
+    // ☣️  Arrange (всякие моки)
+
+    //🔥 Act
+    const res = signFile('');
+
+    //❓ Assert
+    expect(res).toBe(1);
+  });
+});
