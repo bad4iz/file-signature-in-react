a709e7504a3aad0f6b7ab650c2cfa902646402e1
bad4iz <develorer@bad4iz.ru>
2024-02-25 12:26:06 +0400

    js doc

--- src/component/utils/hooks/useDoCertsList.ts
+++ src/component/utils/hooks/useDoCertsList.ts
@@ -6,7 +6,7 @@ import { extract } from '../extract';
 
 /**
  * Hook. This function returns a list of certificate labels and thumbprints using CCPA API.
- * @returns {void}
+ * @returns A list of certificate labels and thumbprints.
  */
 export const useDoCertsList = () =>
   useMemo(async () => {
